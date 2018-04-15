6ea17dc79792b1df762f0a120c964e91c4320eef
Chris Niu <chrniu@berkeley.edu>
2018-04-15 02:55:51 -0700

    helo frens pt 2

---
+++ components/atom.js
@@ -0,0 +1,20 @@
+AFRAME.registerComponent('atom', {
+  schema: {
+    sym: {type: 'string'},
+    radius: {type: 'number'},
+    color: {type: 'color'}
+  },
+  init: function () {
+    let data = this.data;
+    let el = this.el;
+
+    this.geometry = new THREE.SphereBufferGeometry(data.radius, 32, 32);
+    this.material = new THREE.MeshStandardMaterial({color: data.color});
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+    el.setObject3D('mesh', this.mesh);
+
+    // let pos = el.object3D.position.x + ' ' + el.object3D.position.y + ' ' + (el.object3D.position.z);
+    let html = '<a-text value="' + data.sym + '" align="center" color="#000" position="0 0 ' + data.radius +'" wrap-count="5"></a-text>';
+    el.innerHTML = html;
+  }
+})
