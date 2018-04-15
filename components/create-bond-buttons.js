03cd3c9f351f73a8fa89c5bcfce7793bc2186a3f
carolynwang <carolyn.wang@berkeley.edu>
2018-04-15 09:00:44 -0700

    got text to work lol

--- components/create-bond-buttons.js
+++ components/create-bond-buttons.js
@@ -9,12 +9,16 @@ AFRAME.registerComponent('create-bond-buttons', {
 
             var elem = document.createElement('a-box');
             elem.setAttribute('create-bond-on-click', '');
-            elem.setAttribute('position', '0 0 0');
+            elem.setAttribute('position', '0 -1.5 0');
             elem.setAttribute('depth', '0.1');
+            elem.setAttribute('width', '2');
             elem.setAttribute('color', '#EF2D5E');
             menu.appendChild(elem);
             var head = document.createElement('a-text');
             head.setAttribute('value', 'add single bond');
+            head.setAttribute('align', 'center');
+            head.setAttribute('position', '0 -1.5 0.1');
+            head.setAttribute('color', '#000');
             menu.appendChild(head);
 
             el.addEventListener('click', this.hideMenu);
