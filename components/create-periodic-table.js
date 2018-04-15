f5571ff9d4987c6665b18a51947ce01f62d1634c
carolynwang <carolyn.wang@berkeley.edu>
2018-04-15 05:38:58 -0700

    merge

--- components/create-periodic-table.js
+++ components/create-periodic-table.js
@@ -4,75 +4,52 @@ AFRAME.registerComponent('create-periodic-table', {
     init: function () {
         var el = this.el;
         this.createTable = function (event) {
+            var el = this.el;
             var scene = document.querySelector('a-scene');
+            var menu = document.querySelector('#periodic-table');
 
-            var entity = document.createElement('a-box');
-            entity.setAttribute('id', 'periodic-table');
-            entity.setAttribute('visible', 'true');
-            entity.setAttribute('height', '1');
-            entity.setAttribute('width', '2');
-            entity.setAttribute('depth', '0.1');
-            entity.setAttribute('color', '#EF2D5E');
-            entity.setAttribute('opacity', '0.5');
-            entity.setAttribute('position', '0 1 -2');
-            scene.appendChild(entity);
+            var entityH = document.createElement('a-text');
+            entity.setAttribute('create-atom-on-click', 'sym: H; radius: 0.3; color: #AAA');
+            entity.setAttribute('value', 'H');
+            entity.setAttribute('position', '-0.5 0.7 0');
+            menu.appendChild(entityH);
 
-            this.createElems();
+            var entityC = document.createElement('a-text');
+            entityC.setAttribute('create-atom-on-click', 'sym: C; radius: 0.5; color: #AAA');
+            entityC.setAttribute('value', 'C');
+            entityC.setAttribute('position', '0 0.7 0');
+=            menu.appendChild(entityC);
+
+            var entityN = document.createElement('a-text');
+            entity.setAttribute('create-atom-on-click', 'sym: N; radius: 0.5; color: #AAA');
+            entity.setAttribute('value', 'N');
+            entity.setAttribute('position', '0.5 0.7 0');
+            menu.appendChild(entityN);
+
+            var entityO = document.createElement('a-text');
+            entity.setAttribute('create-atom-on-click', 'sym: O; radius: 0.5; color: #AAA');
+            entity.setAttribute('value', 'O');
+            entity.setAttribute('position', '-0.5 0.5 0');
+            menu.appendChild(entityO);
+
+            var entityCl = document.createElement('a-text');
+            entity.setAttribute('create-atom-on-click', 'sym: Cl; radius: 0.7; color: #AAA');
+            entity.setAttribute('value', 'Cl');
+            entity.setAttribute('position', '0 0.5 0');
+            menu.appendChild(entityCl);
+
+            var entityBr = document.createElement('a-text');
+            entity.setAttribute('create-atom-on-click', 'sym: Br; radius: 0.7; color: #AAA');
+            entity.setAttribute('value', 'Br');
+            entity.setAttribute('position', '0.5 0.5 0');
+            menu.appendChild(entityBr);
+
+            el.addEventListener('click', this.hideMenu);
         }
-
-          this.createElems = function() {
-              var el = this.el;
-              var scene = document.querySelector('a-scene');
-              var menu = document.querySelector('#periodic-table');
-
-              var entityH = document.createElement('a-text');
-              entity.setAttribute('create-atom-on-click', 'sym: H; radius: 0.3; color: #AAA');
-              entity.setAttribute('value', 'H');
-              entity.setAttribute('position', '-0.5 0.5 0');
-              entity.setAttribute('color', '#000');
-              menu.appendChild(entityH);
-
-              var entityC = document.createElement('a-text');
-              entityC.setAttribute('create-atom-on-click', 'sym: C; radius: 0.5; color: #AAA');
-              entityC.setAttribute('value', 'C');
-              entityC.setAttribute('position', '0 0.5 0');
-              entityC.setAttribute('color', '#000');
-              menu.appendChild(entityC);
-
-              var entityN = document.createElement('a-text');
-              entity.setAttribute('create-atom-on-click', 'sym: N; radius: 0.5; color: #AAA');
-              entity.setAttribute('value', 'N');
-              entity.setAttribute('position', '0.5 0.5 0');
-              entity.setAttribute('color', '#000');
-              menu.appendChild(entityN);
-
-              var entityO = document.createElement('a-text');
-              entity.setAttribute('create-atom-on-click', 'sym: O; radius: 0.5; color: #AAA');
-              entity.setAttribute('value', 'O');
-              entity.setAttribute('position', '-0.5 0 0');
-              entity.setAttribute('color', '#000');
-              menu.appendChild(entityO);
-
-              var entityCl = document.createElement('a-text');
-              entity.setAttribute('create-atom-on-click', 'sym: Cl; radius: 0.7; color: #AAA');
-              entity.setAttribute('value', 'Cl');
-              entity.setAttribute('position', '0 0 0');
-              entity.setAttribute('color', '#000');
-              menu.appendChild(entityCl);
-
-              var entityBr = document.createElement('a-text');
-              entity.setAttribute('create-atom-on-click', 'sym: Br; radius: 0.7; color: #AAA');
-              entity.setAttribute('value', 'Br');
-              entity.setAttribute('position', '0.5 0 0');
-              entity.setAttribute('color', '#000');
-              menu.appendChild(entityBr);
-
-              el.addEventListener('click', this.hideMenu);
-          }
 
           this.hideMenu = function () {
             var scene = document.querySelector('a-scene');
-            var menu = document.querySelector('#periodic-table');
+            var menu = document.querySelector('#menu');
             scene.removeChild(menu);
          }
 
